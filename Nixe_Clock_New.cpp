d8400101a032a3401b1402e56099228b640cdee2
John Pomeroy <nerd.johnny.boy@gmail.com>
2025-06-24 14:31:31 -0500

    Add files via upload
    First Commit

---
+++ Nixe_Clock_New.cpp
@@ -0,0 +1,185 @@
+/* Arduino-free nixie clock, built with a standalone ATmega328P microcontroller and shift registers
+ * 
+ * Clock has two buttons, to set hours and minutes respectively.
+ * A DS1307 real-time clock is also included in this implementation, allowing for the clock to keep more accurate time and retain it.
+ * If the RTC battery dies, the clock *should* output "0:00"
+ * 
+ * The clock can also output data via serial
+ * 
+ * Chris Green - 1/11/2016
+ * Modified by John Pomeroy
+ */
+
+
+#include <nixie10.h> // my nixie driver library
+#include <Wire.h> // I2C library
+#include "RTClib.h" // adafruit library to drive DS1307 RTC connected via I2C
+
+//WiFi headers
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+//Register Pins
+#define LATCHPIN 3
+#define CLOCKPIN 15 
+#define DATAPIN 2
+
+//Button Pins
+#define HOURBUTTONPIN 5
+#define MINUTEBUTTONPIN 6
+#define WIFIBUTTON 7
+
+#define LEDPIN 13
+
+//SQW PIN 4
+//SCL PIN 22
+//SDA PIN 21
+
+#define CLOCKSETDELAY 400  //I like this amount of delay for setting my clock, but change it as you like
+#define OFF 11 //setting a tube to a value higher than 9 turns it off
+
+int hour_tens, hour_ones, min_tens, min_ones, hours, minutes; //some global variables to hold current time
+
+nixie10 outReg; //tube-register object
+RTC_DS3231 rtc; //real-time-clock object
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; //for serial output
+
+// Replace with your network credentials
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+String formattedDate;
+
+void setup() {
+ outReg.initialize_16reg(LATCHPIN, CLOCKPIN, DATAPIN); //initialize the output register array
+ 
+ pinMode(LEDPIN, OUTPUT);
+ 
+ //clock safety (borrowed from Adafruit example code)
+ //#ifndef ESP8266
+ // while (!Serial); // for Leonardo/Micro/Zero
+ //#endif
+
+ Serial.begin(57600);
+ Serial.println("Setup Started");
+ 
+ //Connecting to WiFi
+ Serial.print("Connecting to ");
+ Serial.println(ssid);
+ WiFi.begin(ssid, password);
+ 
+ while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+// Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(-21600);
+  
+ //Setting up RTC
+ if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  
+ //set clock if RTC is not set
+ if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+}
+
+void loop() {
+  DateTime now = rtc.now(); //take a "snapshot" of the time
+  hours = (now.hour() ? now.hour() : 12); //account for the fact that in 24hr time, this is zero for 12AM
+  minutes = now.minute();  
+
+while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  
+   formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+  String[] NewTime = GetTime(formattedDate); 	
+  // Extract date
+  //int splitT = formattedDate.indexOf("T");
+  //dayStamp = formattedDate.substring(0, splitT);
+  Serial.print("DATE: ");
+  Serial.println(NewTime[0]);
+  // Extract time
+  //timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.print("HOUR: ");
+  Serial.println(NewTime[1]);
+  delay(1000);
+  
+  /*
+  if(digitalRead(HOURBUTTONPIN)){
+    if(hours < 23)
+      hours++;
+    else
+      hours = 1;
+    rtc.adjust(DateTime(now.year(), now.month(), now.day(), hours, minutes, now.second()));
+    delay(CLOCKSETDELAY);
+  }
+
+  if(digitalRead(MINUTEBUTTONPIN)){
+    if(minutes < 59)
+      minutes++;
+    else
+      minutes = 0;
+    rtc.adjust(DateTime(now.year(), now.month(), now.day(), hours, minutes, now.second()));
+    delay(CLOCKSETDELAY);
+  }
+  
+  */
+  int time = millis();
+  if(!(time%1000)){
+    digitalWrite(LEDPIN, HIGH);
+    delay(200);
+    digitalWrite(LEDPIN, LOW);
+  }
+
+  //break up current time into individual digits and convert to 12hr format
+  min_ones = minutes%10;
+  min_tens = (minutes - min_ones)/10;
+  hour_ones = (hours>12) ? (hours-12)%10 : hours%10; //convert to 12hr format
+  hour_tens = (hours>12) ? (hours - hour_ones - 12)/10 : (hours - hour_ones)/10; //convert to 12hr format
+  
+  outReg.set_16reg((hour_tens ? hour_tens : OFF), hour_ones, min_tens, min_ones); //push out the current time to the register array, turning off 10s hour tube if zero.
+    
+}
+
+string[] GetTime(String currentTime)
+{
+	String[] SplitTime = {"",""};
+	String dayStamp;
+	String timeStamp;
+	int splitT = formattedDate.indexOf("T");
+	dayStamp = formattedDate.substring(0, splitT);
+  	timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+	SplitTime [0] = dayStamp;
+	SplitTime [1] = timeStamp;
+	return SplitTime;	
+}
