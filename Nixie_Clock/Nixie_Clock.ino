a9ad69e8459c43762f718a562be1ce0b2a73c7e8
John Pomeroy <nerd.johnny.boy@gmail.com>
2025-07-11 14:11:15 -0500

    Update Nixie_Clock.ino
    Added if power lost so we know if it's first time starting up or battery died.

--- Nixie_Clock/Nixie_Clock.ino
+++ Nixie_Clock/Nixie_Clock.ino
@@ -109,14 +109,14 @@ void setup() {
   }
 
   // set clock if RTC is not set
-  // if (! rtc.isrunning()) {
-  // Serial.println("RTC is NOT running!");
+   if (! rtc.lostPower()) {
+  Serial.println("Power Loss or First Bootup; Setting a default time");
   //  following line sets the RTC to the date & time this sketch was compiled
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  //}
+  }
 }
 
 void loop() {
